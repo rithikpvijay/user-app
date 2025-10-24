a49a469fdbf60135a6c084644335ff19126d756f
Rithik P Vijay <rithikpvijay2002@gmail.com>
2025-10-24 17:28:18 +0530

    notfound updated

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -3,7 +3,7 @@
     <base-card>
       <h2>Page not found</h2>
       <p>
-        This page could not be found - maybe check out all our
+        This page could not be found - maybe check out the
         <router-link to="/">home</router-link>
       </p></base-card
     >
